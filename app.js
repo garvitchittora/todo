587038dd0f1ef5c9fac43778b3ad7d791fe2203c
garvitchittora <54021348+garvitchittora@users.noreply.github.com>
2020-02-07 14:55:17 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,41 @@
+$('.add-todo').on('keypress',function (e) {
+      e.preventDefault
+      if (e.which == 13) {
+           if($(this).val() != ''){
+           var todo = $(this).val();
+            createTodo(todo); 
+            countTodos();
+           }else{
+           }
+      }
+});
+
+$('.todolist').on('change','#sortable li input[type="checkbox"]',function(){
+    if($(this).prop('checked')){
+        var doneItem = $(this).parent().parent().find('label').text();
+        $(this).parent().parent().parent().addClass('remove');
+        done(doneItem);
+        countTodos();
+    }
+});
+$('.todolist').on('click','.remove-item',function(){
+    removeItem(this);
+});
+
+function createTodo(text){
+    var markup = '<li class="ui-state-default"><div class="checkbox"><label><input type="checkbox" value="" />'+ text +'</label></div></li>';
+    $('#sortable').append(markup);
+    $('.add-todo').val('');
+}
+
+function done(doneItem){
+    var done = doneItem;
+    var markup = '<li>'+ done +'<button class="btn btn-default btn-xs pull-right  remove-item"><span class="glyphicon glyphicon-remove"></span></button></li>';
+    $('#done-items').append(markup);
+    $('.remove').remove();
+}
+
+
+function removeItem(element){
+    $(element).parent().remove();
+}
